2be6c129b1769b97a28fbcf0262257e0cf76688b
iNepali <sanjayaghimire@gmail.com>
2017-05-15 12:06:46 -0500

    v1.1

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ socket.on('connection', function(connection){
     console.log("User connected#" + connection.id);
 
     connection.on("message", function(msg){
+        console.log(msg);
         socket.emit("message", msg);
     })
 });
 
 server.listen(port, function(){
     console.log('Server started on port ' + port);
+    console.log('Version: v1.1');
 }); 
